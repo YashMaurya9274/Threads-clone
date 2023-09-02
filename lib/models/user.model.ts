59cf3280ce351595f73653d019e744f2fee1afbc
Yash Maurya <yash.maurya537468@gmail.com>
2023-09-02 23:20:37 +0530

    threads app

---
+++ lib/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+
+// Initially 'mongoose.models.User' so it'll use userSchema
+// But from the second and further times 'mongoose.models.User' will be there in the DB
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
