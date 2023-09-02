a70f272384dc6ad62c3e1360f04a575f20da8875
Yash Maurya <yash.maurya537468@gmail.com>
2023-09-03 01:10:16 +0530

    ignore ts errors

---
+++ lib/models/community.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      red: "User",
+    },
+  ],
+});
+
+// Initially 'mongoose.models.User' so it'll use userSchema
+// But from the second and further times 'mongoose.models.User' will be there in the DB
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
+
+export default Community;
